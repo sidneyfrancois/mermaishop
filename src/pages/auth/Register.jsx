a13f12f5c40117b0b9eeae2d389ed5bd320910b0
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-29 17:17:46 -0300

    Inserindo página de register

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
+import WelcomeCard from "../../components/cards/WelcomeCard";
+import axios from "axios";
+import { useAuth } from "../../contexts/auth";
+
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [auth, setAuth] = useAuth();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/users", {
+        name,
+        email,
+        password,
+      });
+
+      localStorage.setItem("auth", JSON.stringify(data));
+      setAuth({ ...auth, token: data.token, user: data.user });
+    } catch (err) {
+      console.log("Erro ao efetuar o login" + err);
+      console.log(err.response.data.message);
+    }
+  }
+
   return (
-    <>
-      <h1>Register page</h1>
-    </>
+    <div>
+      <WelcomeCard title="Register" />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="form-control mb-4 p-2"
+                placeholder="Digite o seu nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                autoFocus
+              />
+
+              <input
+                type="email"
+                className="form-control mb-4 p-2"
+                placeholder="Digite o seu e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <input
+                type="password"
+                className="form-control mb-4 p-2"
+                placeholder="Digite a sua senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button className="btn btn-primary" type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
