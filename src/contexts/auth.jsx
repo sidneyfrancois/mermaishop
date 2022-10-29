340b41169e1d74925da2015cb49a7a4eba7818ae
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-29 17:10:16 -0300

    Inserindo login de usuário

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,35 +1,31 @@
-import { useEffect } from "react";
-import { createContext, useContext, useState } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
 const AuthContext = createContext();
 
-export function AuthProvider({ children }) {
+const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     user: null,
     token: "",
   });
 
-  axios.defaults.baseURL = process.env.VITE_APP_API;
+  axios.defaults.baseURL = import.meta.env.VITE_APP_API;
 
   useEffect(() => {
     const data = localStorage.getItem("auth");
     if (data) {
       const parsed = JSON.parse(data);
-      setAuth[{ ...auth, user: parsed.user, token: parsed.token }];
+      setAuth({ ...auth, user: parsed.user, token: parsed.token });
     }
   }, []);
 
   return (
-    <>
-      <AuthContext.Provider value={{ auth, setAuth }}>
-        {children}
-      </AuthContext.Provider>
-    </>
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
   );
-}
+};
 
-export function useAuth() {
-  return useContext(AuthContext);
-}
+const useAuth = () => useContext(AuthContext);
 
 export { useAuth, AuthProvider };
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,63 @@
+import WelcomeCard from "../../components/cards/WelcomeCard";
+import axios from "axios";
+import { useAuth } from "../../contexts/auth";
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [auth, setAuth] = useAuth();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/auth", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("auth", JSON.stringify(data));
+      setAuth({ ...auth, token: data.token, user: data.user });
+
+      console.log(auth);
+      console.log(data);
+    } catch (err) {
+      console.log("Erro ao efetuar o login" + err);
+      console.log(err.response.data.message);
+    }
+  }
+
   return (
-    <>
-      <h1>Login Pagw</h1>
-    </>
+    <div>
+      <WelcomeCard title="Página de Login" />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                className="form-control mb-4 p-2"
+                placeholder="Digite o seu e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                className="form-control mb-4 p-2"
+                placeholder="Digite a sua senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <button className="btn btn-primary" type="submit">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
