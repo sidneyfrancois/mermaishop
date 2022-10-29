848338e68bd9f21877823663a3a70152a375c71f
Sidney François <sidney.fvm.ps@outlook.com>
2022-10-28 22:10:43 -0300

    Criando card de produto

--- src/components/cards/ProductCard.jsx
+++ src/components/cards/ProductCard.jsx
@@ -1,11 +1,11 @@
-export default function ProductCard({ p }) {
+export default function ProductCard({ product }) {
   return (
-    <div className="card mb-3 hoverable">
+    <div className="card  hoverable">
       <div className="card-body">
-        <h5>Produto 1</h5>
-        <h4 className="fw-bold">preço</h4>
+        <h5>{product.name}</h5>
+        <h4 className="fw-bold">{product.price}</h4>
 
-        <p className="card-text">descrição</p>
+        <p className="card-text">{product.description}</p>
       </div>
       <div className="d-flex justify-content-between">
         <button className="btn btn-primary col card-button">Detalhes</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,40 @@
+import { useEffect } from "react";
 import ProductCard from "../components/cards/ProductCard";
 import WelcomeCard from "../components/cards/WelcomeCard";
+import axios from "axios";
+import { useState } from "react";
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_API;
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  async function loadProducts() {
+    try {
+      const { data } = await axios.get("/product");
+      setProducts(data);
+      console.log(data);
+    } catch (err) {
+      console.log("Erro no carregamento de produtos: " + err);
+    }
+  }
+
   return (
     <div>
       <WelcomeCard title="Seja bem vindo!" />
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6">
+          <div className="">
             <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
-              <ProductCard />
+              {products?.map((product) => (
+                <div className="col-md-6" key={product.id}>
+                  <ProductCard product={product} />
+                </div>
+              ))}
             </h2>
           </div>
         </div>
